840f429d0b2bab6117f5fc498d311895ecc3f68a
suman.s <suman.s@CPU832.CORP.SYSTECHMNG.NET>
2019-09-24 14:35:02 +0530

    added

--- basicjava/src/understandingconcepts/helloword.java
+++ basicjava/src/understandingconcepts/helloword.java
@@ -3,8 +3,9 @@ package understandingconcepts;
 public class helloword {
 
 	public static void main(String[] args) {
+		//display in the screens
 		System.out.println("Hello");
-		//hhhhhhhhhhhhhhhhhhhhhhhhh
+		
 		
 
 	}
