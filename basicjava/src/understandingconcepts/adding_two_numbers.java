c36ae8393a429b9e0c46dc13d19321fa650ad488
suman.s <suman.s@CPU832.CORP.SYSTECHMNG.NET>
2019-09-26 11:10:01 +0530

    new program

---
+++ basicjava/src/understandingconcepts/adding_two_numbers.java
@@ -0,0 +1,5 @@
+package understandingconcepts;
+
+public class adding_two_numbers {
+
+}
---
+++ basicjava/src/understandingconcepts/displayingnumber.java
@@ -0,0 +1,16 @@
+package understandingconcepts;
+
+public class displayingnumber {
+
+	public static void main(String[] args) {
+	
+		
+		for(int i = 10; i > 0; i--)
+		{
+			System.out.println(i);
+		}
+				
+
+	}
+
+}
